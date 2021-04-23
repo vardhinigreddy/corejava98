e88e27452bffc0a161d443afc8ad92dc525d5feb
vardhinigreddy <53506757+vardhinigreddy@users.noreply.github.com>
2021-04-23 16:59:46 +0530

    changes updated

--- Test.java
+++ Test.java
@@ -1,3 +1,7 @@
+/*Create a class with a method that prints "This is parent class" and its subclass with another method that prints "This is child class". Now, create an object for each of the class and call
+ 1 - method of parent class by object of parent class
+ 2 - method of child class by object of child class
+ 3 - method of parent class by object of child class*/
 package programs;
 class Demo{
 	
